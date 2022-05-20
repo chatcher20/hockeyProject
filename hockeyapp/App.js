fc51021ad5725367b22fbdb481ea208d16635764
Chris Hatcher <hatcherchris20@gmail.com>
2022-05-20 05:46:12 +0000

    adding a logo and a button

--- hockeyapp/App.js
+++ hockeyapp/App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png'; 
 
 export default function App() {
   return (
-    <View style={styles.container}> 
-      <Text style={{color: '#888', fontSize: 18}}> 
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      
+      <Text style={styles.instructions}> 
         The Calgary Flames rock! To share a photo from your phone with a friend, just press the button below!
         </Text>
-      <StatusBar style="auto" />
+
+      <TouchableOpacity onPress={() => alert('Matthew Tkachuk says hello!')} style={styles.button} >
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -19,4 +26,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
 });
+
+
+
+
