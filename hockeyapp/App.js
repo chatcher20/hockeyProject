38c651bda6e4cc12563b410ecebd4cce1d41975b
Chris Hatcher <hatcherchris20@gmail.com>
2022-05-25 03:44:32 +0000

    using the selected image from phone

--- hockeyapp/App.js
+++ hockeyapp/App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { TouchableOpacity, Image, StyleSheet, Text, View } from 'react-native';
 import logo from './assets/logo.png';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = React.useState(null);
 
   let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -15,10 +18,26 @@ export default function App() {
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
     console.log(pickerResult);
 
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
   }
 
 
-  
+
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
@@ -61,6 +80,19 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
   },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"       // see note below
+  }
+  /*
+  You might expect that because we gave our image an equal width 
+  and height it would be a square, but in the above video it's 
+  rectangular. This is because of resizeMode, an image style property 
+  that lets us control how the image is resized to fit the given 
+  dimensions. Try switching it from contain to stretch or cover 
+  to see other behaviors.
+  */
 });
 
 
